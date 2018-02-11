d9bec9d9469d84a5f743a309ea6cf60255e0e479
Wes Chuang <icemilk0122@gmail.com>
2018-02-12 01:48:32 +0800

    for loop

--- src/main.js
+++ src/main.js
@@ -10,33 +10,57 @@ Vue.config.productionTip = false;
 var vm = new Vue({
   el: '#app',
   data: {
-       count1: 0,
-       count2: 0
-  },
-  methods: {
-      thirdFunction () {
-          alert('thirdFunction() is called.');
-      },
-      secondFunction () {
-          alert('secondFunction() is called.');
-      },
-      firstFunction () {
-          alert('firstFunction() is called.');
-      },
-      addCount1 () {
-          this.count1 += 1;
-      },
-      addCount2 () {
-          this.count2 += 1;
-      }
-  }
-});
-var vm2 = new Vue({
-  el: '#app2',
-  methods: {
-      testFunction () {
-          alert('Hello!');
-      }
+      memberArray: [
+          { id: '11', name:'Eva' },
+          { id: '12', name:'Ray' },
+          { id: '13', name:'Ben' }
+      ]
   }
 });
 
+var vm2 = new Vue({
+  el: '#app2',
+  data: {
+    memberObject: {
+      name: 'Eva',
+      gender: 'female',
+      age: '22'
+    }
+  }
+});
+
+vm.memberArray.push({id: '14', name:'John'});
+//filter name is John
+// vm.memberArray = vm.memberArray.filter(function (member) {
+//   return member.name.match(/John/)
+// })
+
+Vue.set(vm2.memberObject, 'weight', '50');
+vm2.memberObject = Object.assign({}, vm2.memberObject, {
+  height: '160',
+  phone: '12345678'
+});
+
+
+var vm3 = new Vue({
+  el: '#app3',
+  data: {
+    numbers: [ 1, 2, 3, 4, 5 ]
+  },
+  //在data就先處理
+  computed: {
+    evenNumbers: function () {
+      return this.numbers.filter(function (number) {
+        return number % 2 === 0
+      })
+    }
+  },
+  //跑迴圈時才處理
+  methods:{
+    even: function (numbers) {
+      return numbers.filter(function (number) {
+        return number % 2 === 0
+      })
+    }
+  }
+});
