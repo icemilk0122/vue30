94510665115cdea7a515d636ab05bb2393a3c776
Wes Chuang <icemilk0122@gmail.com>
2018-02-13 18:37:42 +0800

    v-if v-show

--- src/main.js
+++ src/main.js
@@ -10,57 +10,22 @@ Vue.config.productionTip = false;
 var vm = new Vue({
   el: '#app',
   data: {
-      memberArray: [
-          { id: '11', name:'Eva' },
-          { id: '12', name:'Ray' },
-          { id: '13', name:'Ben' }
-      ]
+      message: 'This is example of v-if',
+      isShow: true
   }
 });
+// 每2秒改變isShow的布林值
+setInterval(function() {
+  vm.isShow = !vm.isShow;
+}, 2000)
 
 var vm2 = new Vue({
   el: '#app2',
   data: {
-    memberObject: {
-      name: 'Eva',
-      gender: 'female',
-      age: '22'
-    }
+      type: 'B'
   }
 });
 
-vm.memberArray.push({id: '14', name:'John'});
-//filter name is John
-// vm.memberArray = vm.memberArray.filter(function (member) {
-//   return member.name.match(/John/)
-// })
-
-Vue.set(vm2.memberObject, 'weight', '50');
-vm2.memberObject = Object.assign({}, vm2.memberObject, {
-  height: '160',
-  phone: '12345678'
-});
-
-
-var vm3 = new Vue({
-  el: '#app3',
-  data: {
-    numbers: [ 1, 2, 3, 4, 5 ]
-  },
-  //在data就先處理
-  computed: {
-    evenNumbers: function () {
-      return this.numbers.filter(function (number) {
-        return number % 2 === 0
-      })
-    }
-  },
-  //跑迴圈時才處理
-  methods:{
-    even: function (numbers) {
-      return numbers.filter(function (number) {
-        return number % 2 === 0
-      })
-    }
-  }
+var vm3 = new Vue ({
+  el: '#app3'
 });
