bf64d98e757f879889862c44f0dc1ac865773cc5
Wes Chuang <icemilk0122@gmail.com>
2018-03-13 11:31:40 +0800

    watch適用時機在資料值一直會有變化的時候

--- src/main.js
+++ src/main.js
@@ -10,42 +10,25 @@ Vue.config.productionTip = false;
 var vm = new Vue({
   el: '#app',
   data: {
-    text: 'hello vue',
-    mytext: 'hello'
+    g: 0,
+    kg: 0,
+    t: 0
   },
-  filters: {
-      toUpperCase(value) {
-          return value.toUpperCase();
-      }
-  }
-}); 
-//全域filters
-Vue.filter('capitalize', function(value) {
-  return value.charAt(0).toUpperCase() + value.slice(1);
-});
-
-var vm2 = new Vue({
-  el: '#app2',
-  data: {
-    message: 'dala!'
-  },
-  computed: {
-    reverseMessage() {
-        return this.message.split('').reverse().join('');
-    }
-  }
-});
-
-var vm3 = new Vue ({
-  el: '#app3',
-  data: {
-    content: '`computed`的功能很強大，它可以對資料做處理，而且它有cache，可以避免重複處理資料，跟`filters`一樣，可以在`computed`裡放入`function`，不同的是，`computed`可以做比較複雜的資料處理。'
-  },
-  computed: {
-    summary() {
-      if (this.content.length > 30)
-        return this.content.slice(0, 27) + '...';
-      return this.content;
+  watch: {
+    g: function(value) {
+      this.g = value;
+      this.kg = value / 1000;
+      this.t = value / 1000 / 1000;
+    },
+    kg: function(value) {
+      this.g = value * 1000;
+      this.kg = value;
+      this.t = value / 1000;
+    },
+    t: function(value) {
+      this.g = value * 1000 * 1000;
+      this.kg = value * 1000;
+      this.t = value;
     }
   }
 });
