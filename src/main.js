e9511535f678101b904d4acd810eb4ac95309f2e
Wes Chuang <icemilk0122@gmail.com>
2018-03-21 16:01:53 +0800

    全域元件與私有元件

--- src/main.js
+++ src/main.js
@@ -1,15 +1,39 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
-import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.component('global-component', {
+  template: '<div><p>{{ message }}</p><button @click="notice">點我</button></div>',
+  data: () => {
+    return {
+      message: '這是全域註冊的元件',
+    }
+  },
+  methods: {
+    notice: () => {
+      alert('全域註冊的元件裡面也可以寫methods!');
+    }
+  }
+})
+
+let local_component = {
+  template: '<div><p>{{ message }}</p><button @click="notice">點我</button></div>',
+  data: () => {
+    return {
+      message: '這是局部註冊的元件'
+    }
+  },
+  methods: {
+    notice: () => {
+      alert('Local Component!');
+    }
+  },
+}
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>',
+  components: { local_component }
 });
