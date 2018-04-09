49abd3a4c2233468c679210bc380048557d0b76b
Wes Chuang <icemilk0122@gmail.com>
2018-04-09 17:02:17 +0800

    slot的三種用法

--- src/main.js
+++ src/main.js
@@ -4,81 +4,38 @@ import Vue from 'vue';
 
 Vue.config.productionTip = false;
 
-Vue.component('child', {
-  props: ['myMessage'],
-  template: '<p>{{ myMessage }}</p>'
+Vue.component('def-child', {
+  template: '<span><slot>This is a default single slot.</slot></span>'
 })
 
-var vm = new Vue ({
-  el: '#app',
-  data: {
-      message: 'HelloWorld'
-  }
+new Vue ({
+  el: '#app'
 })
 
-
-Vue.component('my-profile', {
-  props: {
-    // 指定資料型態為數值
-    id: Number,
-    
-    // 指定多種資料型態
-    password: [String, Number],
-    
-    // 指定為字串型態，且為必要屬性
-    name: {
-      type: String,
-      required: true
-    },
-    
-    // 指定為數值型態，且有預設值
-    age: {
-      type: Number,
-      default: 0,
-      validator: function (value) {
-        return value >= 0
-      }
-    },
-    
-    // 指定為陣列型態，且有預設值，
-    // 物件或陣列的資料型態其預設值由函數回傳值
-    skills: {
-      type: Array,
-      default: function(){
-       return ['nothing']
-      }
-    }
-  },
-  
-  template: '<p>id: {{ id }} <br> password: {{ password }} <br> age: {{ age }} <br> skills: {{ skills }}</p>'
+Vue.component('container', {
+  template: `<div>
+                <header>
+                    <slot name="header"></slot>
+                </header>
+                <main>
+                    <slot></slot>
+                </main>
+                <footer>
+                    <slot name="footer"></slot>
+                </footer>
+             </div>`
 })
 
-new Vue({
+new Vue ({
   el: '#app2'
 })
 
-
-//roll
-Vue.component('dice-button', {
-  template: '<button @click="rollChild">Roll</button>',
-  methods: {
-    rollChild: function(){
-      // 取 1~6 的亂數
-      var value = Math.floor(Math.random() * 6) + 1;
-      // 觸發父元件的事件（如果有的話），並附上值
-      this.$emit('roll', value)
-    }
-  }
+Vue.component('child', {
+  template: `<div>
+                <slot text="This is a text from slot."></slot>	
+            <div>`
 })
 
-new Vue({
-  el: '#app3',
-  data: {
-    value: 1
-  },
-  methods: {
-    showResult: function (v) {
-      this.value = Number(v)
-    }
-  }
+new Vue ({
+  el: '#app3'
 })
